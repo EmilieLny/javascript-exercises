efef9d9485129824bc565022e49c9f1c993c2ddb
Emilie Layani <emilielayani@gmail.com>
2024-06-28 14:26:27 -0400

    try faster

---
+++ SmallestCommonRegion/solution_fast.js
@@ -0,0 +1,25 @@
+const regions = [
+    ["Earth", "North America", "South America"],
+    ["South America", "Brazil"],
+    ["North America", "United States", "Canada"],
+    ["Canada", "Ontario", "Quebec"],
+    ["United States", "New York", "Boston"]
+];
+const region1 = "Quebec"
+const region2 = "New York"
+
+// const regions = [
+//     ["Earth", "North America", "South America"],
+//     ["North America", "United States", "Canada"],
+//     ["United States", "New York", "Boston"],
+//     ["Canada", "Ontario", "Quebec"],
+//     ["South America", "Brazil"]
+// ]
+// const region1 = "Canada"
+// const region2 = "South America"
+
+const findSmallestRegion = (regions, region1, region2) => {
+
+}
+
+console.log(findSmallestRegion(regions, region1, region2))
